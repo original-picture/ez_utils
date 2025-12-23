19c8f4fc928158d035cb7aa63787e02b3fb363e6
Dexter <42780773+original-picture@users.noreply.github.com>
2025-12-22 22:54:49 -0800

    fixed cmake issues and added stuff to ez_math.h

--- ez_math.h
+++ ez_math.h
@@ -1,10 +1,21 @@
 
 #pragma once
 
+#include <algorithm>
+
 namespace ez {
     template<typename float_type>
     float_type lerp(float_type a, float_type b, float_type factor) {
         return (float_type(1)-factor)*a + factor*b;
     }
+
+    float  pi_f = 3.14159265358979323846264f;
+    double pi_d = 3.14159265358979323846264 ;
+
+    template<typename float_type>
+    float_type clamp(float_type number, float_type lower_bound = float_type(-1), float_type upper_bound = float_type(1)) {
+        return std::max(lower_bound, std::min(upper_bound, number));
+    }
 }
+
 
