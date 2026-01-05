33f5289554eedca4acc91f2ef113d4577ab79bd8
Dexter <42780773+original-picture@users.noreply.github.com>
2026-01-04 20:14:46 -0800

    renamed pi_f to fpi

--- ez_math.h
+++ ez_math.h
@@ -9,8 +9,8 @@ namespace ez {
         return (float_type(1)-factor)*a + factor*b;
     }
 
-    float  pi_f = 3.14159265358979323846264f;
-    double pi_d = 3.14159265358979323846264 ;
+    float  fpi = 3.14159265358979323846264f;
+    double dpi = 3.14159265358979323846264 ;
 
     template<typename float_type>
     float_type clamp(float_type number, float_type lower_bound = float_type(-1), float_type upper_bound = float_type(1)) {
