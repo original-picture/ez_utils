7a683ab1d828335d8efbd8c12aaf76d97d3c2493
Dexter <42780773+original-picture@users.noreply.github.com>
2026-06-29 00:28:28 -0700

    minor changes

--- ez_math.h
+++ ez_math.h
@@ -10,8 +10,11 @@ namespace ez {
         return (float_type(1)-factor)*a + factor*b;
     }
 
-    inline static constexpr float  fpi = 3.14159265358979323846264f;
     inline static constexpr double dpi = 3.14159265358979323846264 ;
+    inline static constexpr float  fpi = dpi;
+
+    template<typename float_type>
+    inline static constexpr float_type pi = dpi;
 
     template<typename float_type>
     float_type clamp(float_type number, float_type lower_bound = float_type(-1), float_type upper_bound = float_type(1)) {
